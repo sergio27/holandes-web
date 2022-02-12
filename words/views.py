8f1d9995b782a0ac08b108a1e23bf7a3a0c7006f
Sergio Ibarra Alcalá <ibarra.sergio@gmail.com>
2022-02-12 16:47:23 -0700

    Add category and level filters

--- words/views.py
+++ words/views.py
@@ -5,20 +5,31 @@ from .serializers import WordSerializer, CategorySerializer
 
 
 class WordViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage meals in the database"""
+    """Manage words in the database"""
     queryset = Word.objects.all()
     serializer_class = WordSerializer
 
     def get_queryset(self):
-        """Return objects"""
-        return self.queryset.order_by('category', 'spanish')
+        """Return word objects"""
+        queryset = self.queryset
+
+        level = self.request.query_params.get("level")
+        category = self.request.query_params.get('category')
+
+        if level:
+            queryset = queryset.filter(level=level)
+        if category:
+            category_id = Category.objects.get(name=category)
+            queryset = queryset.filter(category_id=category_id)
+
+        return queryset.order_by('category', 'spanish')
 
 
-class CateogryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage diets in the database"""
+class CategoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage categories in the database"""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        """Return objects"""
+        """Return category objects"""
         return self.queryset.order_by('name')
